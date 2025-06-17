b96b287225aeb52adb68b162c0b6de81a644b5a7
binibinibini <163000187+binibinibini@users.noreply.github.com>
2025-06-17 23:35:08 +0900

    Update minus.py

--- src/myproj/myutil/minus.py
+++ src/myproj/myutil/minus.py
@@ -1,1 +1,6 @@
+def minus(x, y): 
+    return x - y
+
+if __name__ == '__main__':  # 현재 파일이면 실행하겠다. 다른 파일에서 import해서 쓸 땐, 테스트 출력이 안나왔으면 좋겠고, 직접 실행할 땐 예제 실행해보고 싶을때 사용
+    print('minus result =', minus(x = 1, y = 2))
 
