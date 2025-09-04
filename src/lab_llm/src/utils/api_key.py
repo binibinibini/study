663463ddf0a14707faa94e4354bb967f6ed956eb
binibinibini <163000187+binibinibini@users.noreply.github.com>
2025-09-04 15:06:57 +0900

    Update api_key.py

--- src/lab_llm/src/utils/api_key.py
+++ src/lab_llm/src/utils/api_key.py
@@ -1,1 +1,14 @@
+import os
+from dotenv import load_dotenv
 
+def get_openai_api_key():
+    # .env 파일의 내용을 읽어서 OS 환경변수(key-value)로 저장.
+    load_dotenv()
+    # OS 환경 변수로 저장된 값을 가져옴.
+    api_key = os.getenv("OPENAI_API_KEY")
+    return api_key
+
+if __name__ == "__main__":
+    # load_dotenv()
+    api_key = get_openai_api_key()
+    print(api_key)
