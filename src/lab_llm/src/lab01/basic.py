39ec7f8d75209f272348c756d2163bbbf415b1fe
binibinibini <163000187+binibinibini@users.noreply.github.com>
2025-09-04 15:01:08 +0900

    Update basic.py

--- src/lab_llm/src/lab01/basic.py
+++ src/lab_llm/src/lab01/basic.py
@@ -25,8 +25,17 @@ if __name__ == '__main__'  # 현재 파일을 메인으로 실행할 때
           }
         ]
     )
+    print(response)     # ChatCompletion 객체
+    print('-' * 10)
+    print(response.choices[0])  # Choice 객체
+    print('-' * 10)
+    print(response.choices[0].message)  # ChatCompletionMessage 객체
+    print('-' * 10)
+    print(response.choices[0].message.content)  # GPT가 생성한 답변
 
-```md
+
+
+```결과
 C:\workspace\lab_llm\.venv\Scripts\python.exe C:\workspace\lab_llm\src\lab01\basic.py 
 ChatCompletion(id='chatcmpl-CBxFKLNhoBZmKQHuBbfAuBArzePac', choices=[Choice(finish_reason='stop', index=0, logprobs=None, message=ChatCompletionMessage(content='당신은 AI 비서와 대화하고 있습니다. 제가 도와드릴 수 있는 것이 있다면 말씀해 주세요!', refusal=None, role='assistant', annotations=[], audio=None, function_call=None, tool_calls=None))], created=1756965386, model='gpt-4o-mini-2024-07-18', object='chat.completion', service_tier='default', system_fingerprint='fp_560af6e559', usage=CompletionUsage(completion_tokens=26, prompt_tokens=31, total_tokens=57, completion_tokens_details=CompletionTokensDetails(accepted_prediction_tokens=0, audio_tokens=0, reasoning_tokens=0, rejected_prediction_tokens=0), prompt_tokens_details=PromptTokensDetails(audio_tokens=0, cached_tokens=0)))
 ----------
