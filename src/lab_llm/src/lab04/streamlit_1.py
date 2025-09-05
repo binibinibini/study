d8daddd35cb1511e28b639d0a5b3d612bb21026f
binibinibini <163000187+binibinibini@users.noreply.github.com>
2025-09-05 11:03:33 +0900

    Update streamlit_1.py

--- src/lab_llm/src/lab04/streamlit_1.py
+++ src/lab_llm/src/lab04/streamlit_1.py
@@ -1,1 +1,51 @@
+# streamlit 사용 방법 보기(터미널에 작성)
+streamlit --help
 
+```
+<결과>
+(.venv) PS C:\workspace\lab_llm> streamlit --help  
+Usage: streamlit [OPTIONS] COMMAND [ARGS]...
+
+  Try out a demo with:
+
+      $ streamlit hello
+
+  Or use the line below to run your own script:
+
+      $ streamlit run your_script.py
+
+Options:
+  --log_level [error|warning|info|debug]
+  --version                       Show the version and exit.
+  --help                          Show this message and exit.
+
+Commands:
+  activate  Activate Streamlit by entering your email.
+  cache     Manage the Streamlit cache.
+  config    Manage Streamlit's config settings.
+  docs      Show help in browser.
+  hello     Runs the Hello World script.
+  help      Print this help message.
+  init      Initialize a new Streamlit project.
+  run       Run a Python script, piping stderr to Streamlit.
+  version   Print Streamlit's version number.
+```
+
+# 페이지 열기
+streamlit run ./src/lab04/streamlit_1.py
+
+```
+<결과>
+  You can now view your Streamlit app in your browser.
+
+  Local URL: http://localhost:8501
+  Network URL: http://192.168.31.11:8501
+```
+
+# 192.168.31.11 건물을 찾을 수 있는 번호. 8501 건물에 들어갈 수 있는 번호(포트번호)
+
+
+# 사이드와 두개의 패널을 갖는 사이트
+streamlit run ./src/lab04/streamlit_2.py
+
+image1 = Image.open('C:\Users\itwill\Desktop\스크린샷 2025-09-05 110121.png')
